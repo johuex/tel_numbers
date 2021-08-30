986ad765a265250d45fd214e9c78590523d97803
Dimitry Polyakov <polds@list.ru>
2021-08-30 13:34:57 +0300

    generator's time optimization on 30%

--- generator.py
+++ generator.py
@@ -1,8 +1,7 @@
 import os
 from config import ROOT_DIR
 from time import time
-from sys import getsizeof
-from random import shuffle
+from random import shuffle, randint, choice
 
 
 def gen():
@@ -11,15 +10,24 @@ def gen():
     file = open(os.path.join(ROOT_DIR, 'gen_numbers.txt'), 'w')
     for i in range(27):
         if i == 26:
-            numbers = [j for j in range(9000000000 + 37037037*i, 9000000000 + 37037037*(i+1)+1)]
+            numbers = [str(j) + '\n' for j in range(9000000000 + 37037037*i, 9000000000 + 37037037*(i+1)+1)]
         else:
-            numbers = [j for j in range(9000000000 + 37037037*i, 9000000000 + 37037037*(i+1))]
-        shuffle(numbers)
-        shuffle(numbers)
-        for j in numbers:
-            file.write("8" + str(j) + "\n")
+            numbers = [str(j) + '\n' for j in range(9000000000 + 37037037*i, 9000000000 + 37037037*(i+1))]
+        # print("Generating number: {} seconds".format(time() - start_time))
+        shuffle_time = time()
+        len_num = len(numbers) - 1
+        for j in range(len_num//2):
+            end_i = len_num-j
+            rand_i = randint(0, end_i//2-1)
+            numbers[end_i], numbers[rand_i] = numbers[rand_i], numbers[end_i]
+        # print("Shuffle list: {} seconds".format(time() - shuffle_time))
+        write_time = time()
+        file.writelines(numbers)
+        # print("Write time: {} seconds".format(time() - write_time))
+        clear_time = time()
         numbers.clear()
+        # print("Clear time: {} seconds".format(time() - clear_time))
 
-    print('Phone numbers were generated in {} minutes!'.format((time() - start_time)/60))
+    print('Phone numbers were generated in {} seconds!'.format((time() - start_time)))
     print()
     file.close()
